cb1417b90bd68ff06cf9eb4f1e6cba0c7330cb96
SmBernardo <bemelo8@gmail.com>
2025-10-24 17:37:16 -0300

    refactor(catalogo): Renderiza livros dinamicamente com *ngFor e LivroService

--- src/app/pages/catalogo/catalogo.ts
+++ src/app/pages/catalogo/catalogo.ts
@@ -1,36 +1,31 @@
-// src/app/pages/catalogo/catalogo.ts
-
-import { Component, OnInit } from '@angular/core'; // 1. Importe OnInit
+import { Component, OnInit } from '@angular/core';
+import { LivroService } from '../../services/livro';
 import { CommonModule } from '@angular/common';
-import { RouterLink, ActivatedRoute } from '@angular/router'; // 2. Importe ActivatedRoute
-import { Livro } from '../../models/livro.model'; // 3. Importe o modelo
-import { LivroService } from '../../services/livro'; // 4. Importe o serviço
+import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
+import { Livro } from '../../models/livro.model';
 
 @Component({
   selector: 'app-catalogo',
-  standalone: true,
-  imports: [CommonModule, RouterLink], // 5. CommonModule é OBRIGATÓRIO para *ngFor
   templateUrl: './catalogo.html',
+  imports: [CommonModule],
   styleUrls: ['./catalogo.css']
 })
-export class CatalogoComponent implements OnInit { // 6. Implemente OnInit
-
-  // 7. Crie uma propriedade para guardar sua lista
+export class CatalogoComponent implements OnInit {
   listaDeLivros: Livro[] = [];
 
-  // 8. Injete o Serviço e o ActivatedRoute
   constructor(
     private livroService: LivroService,
-    private route: ActivatedRoute
-  ) { }
+    private sanitizer: DomSanitizer
+  ) {}
 
-  // 9. ngOnInit é chamado quando o componente é carregado
   ngOnInit(): void {
-    // Fica "ouvindo" qualquer mudança no parâmetro 'q' da URL
-    this.route.queryParams.subscribe(params => {
-      const termoBusca = params['q']; // Pega o termo da busca (ex: "stephen king")
-      // Chama o serviço para pegar os livros (filtrados ou todos)
-      this.listaDeLivros = this.livroService.getLivros(termoBusca);
-    });
+    // Busca todos os livros do serviço
+    this.listaDeLivros = this.livroService.getLivros();
+  }
+
+  // Garante que o Angular aceite o estilo como seguro
+  getSafeImageUrl(url: string): SafeStyle {
+    const style = `url('${url}') center / cover`;
+    return this.sanitizer.bypassSecurityTrustStyle(style);
   }
 }
