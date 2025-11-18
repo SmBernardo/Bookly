95b6c8ec08111a7eaeb23ef07d1c9c2997bf486c
SmBernardo <bemelo8@gmail.com>
2025-11-17 22:17:06 -0300

    fix(router): Habilita 'onSameUrlNavigation' para forçar recarregamento na busca

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,19 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter,ExtraOptions } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+const routerOptions: ExtraOptions = {
+  onSameUrlNavigation: 'reload'
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay()),
-    provideHttpClient(withFetch())
+    provideRouter(routes, routerOptions),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(withFetch()),
   ]
 };
