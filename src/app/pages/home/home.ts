a61a8811543bcef697febbee1f01797cbeaf1136
SmBernardo <bemelo8@gmail.com>
2025-11-18 14:36:09 -0300

    feat(home): Alterna botão de login para ícone de perfil quando usuário está logado

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,33 +1,42 @@
 // src/app/pages/home/home.ts
 
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterLink
-  ],
+  imports: [CommonModule, RouterLink],
   templateUrl: './home.html',
   styleUrls: ['./home.css']
 })
-export class HomeComponent {
+
+export class HomeComponent implements OnInit {
+
+  usuarioLogado: boolean = false;
 
   constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    if (localStorage.getItem('usuarioLogado')) {
+      this.usuarioLogado = true;
+    }
+  }
 
   irParaLogin() {
     this.router.navigate(['/login']);
   }
 
-  buscar(termo: string): void {
-        if (termo && termo.trim() !== '') {
+  irParaPerfil() {
+    this.router.navigate(['/perfil']);
+  }
 
-            this.router.navigate(['/catalogo'], { queryParams: { q: termo } });
-        } else {
-            this.router.navigate(['/catalogo']);
-        }
+  buscar(termo: string): void {
+    if (termo && termo.trim() !== '') {
+      this.router.navigate(['/catalogo'], { queryParams: { q: termo } });
+    } else {
+      this.router.navigate(['/catalogo']);
     }
+  }
 }
